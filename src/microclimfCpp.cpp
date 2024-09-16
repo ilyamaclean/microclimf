a79cd39b2d04a84956dc3924e6e65265e0df6b16
Ilya Maclean <i.m.d.maclean@exeter.ac.uk>
2024-09-16 12:32:47 +0100

    fix to negative relative humidity in weather height adjustment

--- src/microclimfCpp.cpp
+++ src/microclimfCpp.cpp
@@ -870,8 +870,8 @@ DataFrame weatherhgtCpp(DataFrame obstime, DataFrame climdata, double zin, doubl
     std::vector<double> Tz(tc.size());
     std::vector<double> Rh(tc.size());
     std::vector<double> Uz(tc.size());
+    // temporary variables to diagnose
     double d = zeroplanedisCpp(0.12, 1);
-    std::vector<double> zm(tc.size());
     for (size_t i = 0; i < tc.size(); ++i) {
         double zm = roughlengthCpp(0.12, 1, d, psih[i]);
         double zh = 0.2 * zm;
@@ -880,12 +880,13 @@ DataFrame weatherhgtCpp(DataFrame obstime, DataFrame climdata, double zin, doubl
         Tz[i] = (Tc[i] - tc[i]) * (1 - lnr) + tc[i];
         // Humidity
         double ea = satvapCpp(tc[i]) * rh[i] / 100;
-        double es = satvapCpp(Tc[i]);
+        double es = satvapCpp(Tc[i]) * sqrt(rh[i] / 100);
         double ez = ea + (es - ea) * (1 - lnr);
         es = satvapCpp(Tz[i]);
         Rh[i] = (ez / es) * 100;
         // Cap Rh
-        if (Rh[i] < 0.75 * rh[i]) Rh[i] = 0.75 * rh[i];
+        if (Rh[i] < 0.25 * rh[i]) Rh[i] = 0.25 * rh[i];
+        if (Rh[i] > 100.0) Rh[i] = 100.0;
         // Wind speed
         double lnru = log((zout - d) / zm) / log((uzin - d) / zm);
         Uz[i] = ws[i] * lnru;
@@ -895,6 +896,8 @@ DataFrame weatherhgtCpp(DataFrame obstime, DataFrame climdata, double zin, doubl
     climdata_copy["temp"] = Tz;
     climdata_copy["relhum"] = Rh;
     climdata_copy["windspeed"] = Uz;
+    climdata_copy["psih"] = psih;
+    climdata_copy["Tc"] = Tc;
     return climdata_copy;
 }
 // Soil moisture model
